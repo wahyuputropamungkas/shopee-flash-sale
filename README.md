14686a69d3fc57b49c7bd8d77a6b73435cc7b21f
Wahyu Putro Pamungkas <wahyuputropamungkas@gmail.com>
2023-04-09 05:44:27 +0700

    input data

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # shopee-flash-sale
+
+Shopee auto checkout (currently with Bank Mandiri payment method)
--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
-import os
 import json
 import time
-from bs4 import BeautifulSoup
+from termcolor import colored
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
@@ -10,15 +9,68 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 
 timeout = 60
-url = 'https://shopee.co.id/SANDAL-DISTRO-PRIA-CASUAL-KEKINIAN-i.367732093.16669092439'
+global inputUrl
+global inputIsProductHasVariations
+global inputTotalProductVariations
+global productVariations
+inputUrl = ''
+inputIsProductHasVariations = 'n'
+inputTotalProductVariations = '0'
+productVariations = []
+
+def inputData():
+    global inputUrl
+    global inputIsProductHasVariations
+    global inputTotalProductVariations
+    global productVariations
+
+    inputUrl = input(colored('PASTE URL (Ctrl + Shift + C) : ', 'red', attrs=['reverse']))
+    inputIsProductHasVariations = input(colored('IS PRODUCT HAS VARIATIONS? (y/n) : ', 'red', attrs=['reverse']))
+
+    productVariations = []
+
+    if inputIsProductHasVariations == 'y':
+        if inputTotalProductVariations.isnumeric():
+            inputTotalProductVariations = input(colored('TOTAL PRODUCT VARIATIONS : ', 'red', attrs=['reverse']))
+
+            for index in range(int(inputTotalProductVariations)):
+                item = input(colored('VARIATION ' + str(index + 1) + ' : ', 'green', attrs=['reverse']))
+
+                productVariations.append(item)
+
+    print(colored('CONFIRMATION! : ', 'blue', attrs=['reverse']))
+    print(colored('YOUR PRODUCT URL IS : ' + inputUrl, 'blue', attrs=['reverse']))
+    print(colored('IS PRODUCT HAS VARIATIONS? : ' + inputIsProductHasVariations, 'blue', attrs=['reverse']))
+
+    if inputIsProductHasVariations == 'y':
+        print(colored('PRODUCT VARIATIONS : ' + str(productVariations), 'blue', attrs=['reverse']))
+
+    isContinue = input(colored('DO YOU WANT TO CONTINUE? (y/n) : ', 'red', attrs=['reverse']))
+
+    if isContinue == 'y':
+        scrap()
+    else:
+        isTryAgain = input(colored('DO YOU WANT TO TRY AGAIN? (y/n) : ', 'red', attrs=['reverse']))
+
+        if isTryAgain == 'y':
+            inputData()
+        else:
+            print(colored('PROGRAM FINISH : ', 'white', attrs=['reverse']))
 
 def scrap():
+    global inputUrl
+    global productVariations
+
+    print(colored('PROCESS : load page ', 'green', attrs=['reverse']))
+
     options = webdriver.ChromeOptions()
     options.add_argument('log-level=3')
-    options.add_argument("--window-size=1280,720")
+    # options.add_argument("--window-size=1280,720")
     driver = webdriver.Chrome(options=options)
 
-    driver.get(url)
+    driver.get(inputUrl)
+
+    print(colored('PROCESS : load cookies ', 'green', attrs=['reverse']))
 
     try:
         with open('./cookie/cookie.json', 'r') as file:
@@ -31,45 +83,82 @@ def scrap():
 
                 driver.add_cookie(cookie)
     except Exception as e:
-        print('-------------------- ' + str(e))
+        print('ERROR loading cookies! ' + str(e))
+
+    print(colored('PROCESS : refreshing ', 'green', attrs=['reverse']))
 
     driver.refresh()
     time.sleep(5)
 
+    waitShippingButton = False
+
     try:
         WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, '//div[text()="Pengiriman ke"]')))
+        waitShippingButton = True
+        print(colored('PROCESS : shipping button ok ', 'green', attrs=['reverse']))
     except Exception as e:
-        print(str(e))
+        print(colored('ERROR waiting for shipping choices! ' + str(e), 'red', attrs=['reverse']))
 
-    driver.find_element(By.XPATH, '//button[contains(@class, "product-variation")][text()="Hitam"]').click()
+    if not waitShippingButton:
+        return True
 
-    driver.find_element(By.XPATH, '//button[contains(@class, "product-variation")][text()="40"]').click()
+    if len(productVariations) > 0:
+        for item in productVariations:
+            driver.find_element(By.XPATH, '//button[contains(@class, "product-variation")][text()="' + item + '"]').click()
 
     driver.find_element(By.XPATH, '//button[text()="beli sekarang"]').click()
 
+    waitCheckoutButton = False
+
     try:
         WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, '//button[contains(@class, "shopee-button-solid shopee-button-solid--primary")]/span[text()="checkout"]')))
+        waitCheckoutButton = True
+        print(colored('PROCESS : checkout button ok ', 'green', attrs=['reverse']))
     except Exception as e:
-        print(str(e))
+        print(colored('ERROR waiting for checkout button! ' + str(e), 'red', attrs=['reverse']))
+
+    if not waitCheckoutButton:
+        return True
 
     driver.find_element(By.XPATH, '//button[contains(@class, "shopee-button-solid shopee-button-solid--primary")]/span[text()="checkout"]').click()
 
+    waitPlaceOrderButton = False
+
     try:
         WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, '//button[text()="Buat Pesanan"]')))
+        waitPlaceOrderButton = True
+        print(colored('PROCESS : place order button ok ', 'green', attrs=['reverse']))
     except Exception as e:
-        print(str(e))
+        print(colored('ERROR waiting for place order button! ' + str(e), 'red', attrs=['reverse']))
+
+    if not waitPlaceOrderButton:
+        return True
+
+    waitBankTransferButton = False
 
     try:
         WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, '//button[text()="Transfer Bank"]')))
+        waitBankTransferButton = True
+        print(colored('PROCESS : bank transfer button ok ', 'green', attrs=['reverse']))
     except Exception as e:
-        print(str(e))
+        print(colored('ERROR waiting for bank transfer button! ' + str(e), 'red', attrs=['reverse']))
+
+    if not waitBankTransferButton:
+        return True
 
     driver.find_element(By.XPATH, '//button[text()="Transfer Bank"]').click()
 
+    waitBankMandiriButton = False
+
     try:
         WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, '//div[contains(@class, "checkout-bank-transfer-item__title") and text()="Bank Mandiri"]')))
+        waitBankMandiriButton = True
+        print(colored('PROCESS : bank mandiri button ok ', 'green', attrs=['reverse']))
     except Exception as e:
-        print(str(e))
+        print(colored('ERROR waiting for bank mandiri button! ' + str(e), 'red', attrs=['reverse']))
+
+    if not waitBankMandiriButton:
+        return True
 
     driver.find_element(By.XPATH, '//div[contains(@class, "checkout-bank-transfer-item__title") and text()="Bank Mandiri"]').click()
 
@@ -77,6 +166,6 @@ def scrap():
 
     driver.find_element(By.XPATH, '//button[text()="Buat Pesanan"]').click()
 
-    print('finish')
+    print(colored('PROGRAM SUCCESS! : ', 'green', attrs=['reverse']))
 
-scrap()
+inputData()
