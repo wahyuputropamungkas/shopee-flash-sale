4a452329cf7bb58663e30ee61d82518279f358e1
Wahyu Putro Pamungkas <wahyuputropamungkas@gmail.com>
2023-04-08 15:08:39 +0700

    init 2

--- app/main.py
+++ app/main.py
@@ -10,15 +10,12 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 
 timeout = 60
-url = 'https://shopee.co.id/Terwuwu-kemeja-panjang-pria-cowok-gatlemen-cowok-biru-navy-dongker-polos-formal-kantor-i.193771301.16641731058'
-productName = 'kemeja panjang pria'
-productUrl = 'kemeja-panjang-pria'
+url = 'https://shopee.co.id/SANDAL-DISTRO-PRIA-CASUAL-KEKINIAN-i.367732093.16669092439'
 
 def scrap():
     print('load homepage...')
 
     options = webdriver.ChromeOptions()
-    # options.add_argument('--headless')
     options.add_argument("--window-size=1280,720")
     driver = webdriver.Chrome(options=options)
 
@@ -40,92 +37,47 @@ def scrap():
     driver.refresh()
     time.sleep(5)
 
-    # waitFlashSaleLink = WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, '//a[contains(@class, "shopee-header-section__header-link")]')))
-
-    # driver.find_element(By.XPATH, '//a[contains(@class, "shopee-header-section__header-link")][contains(@href, "/flash_sale")]').click()
-
-    # try:
-    #     waitProductLink = WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, '//a[contains(@href, "' + productUrl + '")]')))
-    # except Exception as e:
-    #     print(str(e))
-
-    # print('-------------- A1 ------------------')
-    # print(driver.current_url)
-
-    # driver.find_element(By.XPATH, '//a[contains(@href, "' + productUrl + '")]').click()
-
-    # try:
-    #     waitBuyButton = WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, '//button[text()="beli sekarang"]')))
-    # except Exception as e:
-    #     print(str(e))
-
     try:
-        waitShippingButton = WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, '//div[text()="Pengiriman ke"]')))
+        WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, '//div[text()="Pengiriman ke"]')))
     except Exception as e:
         print(str(e))
 
-    print('-------------- A2 ------------------')
-    print(driver.current_url)
+    driver.find_element(By.XPATH, '//button[contains(@class, "product-variation")][text()="Hitam"]').click()
 
-    driver.find_element(By.XPATH, '//button[contains(@class, "product-variation")][text()="Navy Pendek"]').click()
-
-    driver.find_element(By.XPATH, '//button[contains(@class, "product-variation")][text()="M"]').click()
+    driver.find_element(By.XPATH, '//button[contains(@class, "product-variation")][text()="40"]').click()
 
     driver.find_element(By.XPATH, '//button[text()="beli sekarang"]').click()
 
-    # try:
-    #     waitRedirect = WebDriverWait(driver, timeout).until(ec.url_changes(url))
-    # except Exception as e:
-    #     print(str(e))
-
-    # print('-------------- A3 ------------------')
-    # print(driver.current_url)
-
     try:
-        waitCheckoutButton = WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, '//button[contains(@class, "shopee-button-solid shopee-button-solid--primary")]/span[text()="checkout"]')))
+        WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, '//button[contains(@class, "shopee-button-solid shopee-button-solid--primary")]/span[text()="checkout"]')))
     except Exception as e:
         print(str(e))
-
-    print('-------------- A4 ------------------')
-    print(driver.current_url)
 
     driver.find_element(By.XPATH, '//button[contains(@class, "shopee-button-solid shopee-button-solid--primary")]/span[text()="checkout"]').click()
 
     try:
-        waitOrderButton = WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, '//button[text()="Buat Pesanan"]')))
+        WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, '//button[text()="Buat Pesanan"]')))
     except Exception as e:
         print(str(e))
-
-    print('-------------- A5 ------------------')
-    print(driver.current_url)
 
     try:
-        waitPaymentMethodButton = WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, '//button[text()="Transfer Bank"]')))
+        WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, '//button[text()="Transfer Bank"]')))
     except Exception as e:
         print(str(e))
-
-    print('-------------- A6 ------------------')
-    print(driver.current_url)
 
     driver.find_element(By.XPATH, '//button[text()="Transfer Bank"]').click()
 
     try:
-        waitBankButton = WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, '//div[contains(@class, "checkout-bank-transfer-item__title") and text()="Bank Mandiri"]')))
+        WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, '//div[contains(@class, "checkout-bank-transfer-item__title") and text()="Bank Mandiri"]')))
     except Exception as e:
         print(str(e))
 
     driver.find_element(By.XPATH, '//div[contains(@class, "checkout-bank-transfer-item__title") and text()="Bank Mandiri"]').click()
 
-    # try:
-    #     waitRadioButton = WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, '//div[contains(@class, "stardust-radio-button stardust-radio-button--checked")]')))
-    # except Exception as e:
-    #     print(str(e))
-
     time.sleep(3)
 
     driver.find_element(By.XPATH, '//button[text()="Buat Pesanan"]').click()
 
-    print('-------------- A7 ------------------')
-    print(driver.current_url)
+    print('finish')
 
 scrap()
